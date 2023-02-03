b2cb667ab48b79f16de0da6988871105247333cd
Fokko Driesprong <fokko@apache.org>
2023-02-03 13:39:58 -0800

    Python: Avoid local imports in test
    My isort seems to mess up the imports, and it is a good
    thing to avoid in general. Prefer fixtures over imports.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -25,12 +25,15 @@ and the built-in pytest fixture request should be used as an additional argument
 retrieved using `request.getfixturevalue(fixture_name)`.
 """
 import os
+import string
+from random import choice
 from tempfile import TemporaryDirectory
 from typing import (
     Any,
     Callable,
     Dict,
     Generator,
+    List,
 )
 from unittest.mock import MagicMock
 from urllib.parse import urlparse
@@ -45,6 +48,7 @@ import botocore.awsrequest
 import botocore.model
 import pytest
 from moto import mock_glue, mock_s3
+from tests.catalog.test_base import InMemoryCatalog
 
 from pyiceberg import schema
 from pyiceberg.io import OutputFile, OutputStream, fsspec
@@ -65,7 +69,6 @@ from pyiceberg.types import (
     StringType,
     StructType,
 )
-from tests.catalog.test_base import InMemoryCatalog
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
@@ -1271,3 +1274,31 @@ def adlfs_fsspec_fileio(request: pytest.FixtureRequest) -> Generator[FsspecFileI
 def empty_home_dir_path(tmp_path_factory: pytest.TempPathFactory) -> str:
     home_path = str(tmp_path_factory.mktemp("home"))
     return home_path
+
+
+RANDOM_LENGTH = 20
+NUM_TABLES = 2
+
+
+@pytest.fixture()
+def table_name() -> str:
+    prefix = "my_iceberg_table-"
+    random_tag = "".join(choice(string.ascii_letters) for _ in range(RANDOM_LENGTH))
+    return (prefix + random_tag).lower()
+
+
+@pytest.fixture()
+def table_list(table_name: str) -> List[str]:
+    return [f"{table_name}_{idx}" for idx in range(NUM_TABLES)]
+
+
+@pytest.fixture()
+def database_name() -> str:
+    prefix = "my_iceberg_database-"
+    random_tag = "".join(choice(string.ascii_letters) for _ in range(RANDOM_LENGTH))
+    return (prefix + random_tag).lower()
+
+
+@pytest.fixture()
+def database_list(database_name: str) -> List[str]:
+    return [f"{database_name}_{idx}" for idx in range(NUM_TABLES)]
